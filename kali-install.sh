892c25751a57277ac4cd93d060979fc6466b95f5
Alan Watson <placebo_al@hotmail.com>
2019-12-27 22:23:23 +0000

    Initial Commit
    This file is to allow faster and more consistent set up for my Kali boxes

---
+++ kali-install.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+## This file is to allow faster and more consistent set up for kali
+## Make sure that Notes is mounted
+
+cd rc-files
+cp bashrc ~/.bashrc
+cp bash-aliases ~/.bash-aliases
+cp screenrc ~/.screenrc
+cp vimrc ~/.vimrc
+cd ..
+cp placeboal111.ovpn ~/Documents/
+
+### Changing the default ssh keys
+cd /etc/ssh/
+mkdir defaultsshkeys
+mv ssh_host_* defaultsshkeys/
+dpkg-reconfigure openssh-server
+
+
+### Files to install with apt
+apt update && apt -y upgrade && apt -y autoremove
+apt install terminator, steghide
+
+
+### Files to install via git
+cd /opt
+# nmap automator - at least until I make my own
+git clone https://github.com/21y4d/nmapAutomator.git
+
+# Windows exploit suggester might also look at other bitsadmin programs
+git clone https://github.com/bitsadmin/wesng.git
+
+# Linenum
+git clone https://github.com/rebootuser/LinEnum.git
+
+### Files to download with wget 
+# pspy 32 and 64 bit
+wget https://github.com/DominicBreuker/pspy/releases/download/v1.2.0/pspy32s
+wget https://github.com/DominicBreuker/pspy/releases/download/v1.2.0/pspy64s
+
+# unix-privesc-check
+wget http://pentestmonkey.net/tools/unix-privesc-check/unix-privesc-check-1.4.tar.gz
