b127236aaf9bbcf804fd0ba86bcd7a7765f08f9b
Alan Watson <placebo_al@hotmail.com>
2020-01-03 05:34:26 +0000

    Update kali-install.sh
    Changed the folder from where it runs, and removed some superfluous lines

--- kali-install.sh
+++ kali-install.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 
 ## This file is to allow faster and more consistent set up for kali
-## Make sure that Notes is mounted
 
-cd rc-files
-cp bashrc ~/.bashrc
-cp bash-aliases ~/.bash-aliases
-cp screenrc ~/.screenrc
-cp vimrc ~/.vimrc
-cd ..
-cp placeboal111.ovpn ~/Documents/
+mv ~/.bashrc ~/bashrc-bak     # save the original until I'm happy with mine
+cp ./bashrc ~/.bashrc
+cp ./bash-aliases ~/.bash-aliases
+cp ./screenrc ~/.screenrc
+cp ./vimrc ~/.vimrc
+# cp .ovpn ~/Documents/
 
 ### Changing the default ssh keys
 cd /etc/ssh/
@@ -28,16 +26,16 @@ cd /opt
 # nmap automator - at least until I make my own
 git clone https://github.com/21y4d/nmapAutomator.git
 
-# Windows exploit suggester might also look at other bitsadmin programs
-git clone https://github.com/bitsadmin/wesng.git
-
-# Linenum
-git clone https://github.com/rebootuser/LinEnum.git
-
-### Files to download with wget 
-# pspy 32 and 64 bit
-wget https://github.com/DominicBreuker/pspy/releases/download/v1.2.0/pspy32s
-wget https://github.com/DominicBreuker/pspy/releases/download/v1.2.0/pspy64s
-
-# unix-privesc-check
-wget http://pentestmonkey.net/tools/unix-privesc-check/unix-privesc-check-1.4.tar.gz
+# Windows exploit suggester might also look at other bitsadmin programs                                            
+git clone https://github.com/bitsadmin/wesng.git                                                                   
+                                                                                                                   
+# Linenum                                                                                                          
+git clone https://github.com/rebootuser/LinEnum.git                                                                
+                                                                                                                   
+### Files to download with wget                                                                                    
+# pspy 32 and 64 bit                                                                                               
+wget https://github.com/DominicBreuker/pspy/releases/download/v1.2.0/pspy32s                                       
+wget https://github.com/DominicBreuker/pspy/releases/download/v1.2.0/pspy64s                                       
+                                                                                                                   
+# unix-privesc-check                                                                                               
+wget http://pentestmonkey.net/tools/unix-privesc-check/unix-privesc-check-1.4.tar.gz 
